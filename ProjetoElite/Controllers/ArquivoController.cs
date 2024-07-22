c49955f48a9be6ba6de2dd6c628e6a12c94bbe28
Jonathas Ribeiro Azer <112482377+j-azer@users.noreply.github.com>
2024-07-22 18:17:48 +0100

    #3 - Ajuste front

--- ProjetoElite/Controllers/ArquivoController.cs
+++ ProjetoElite/Controllers/ArquivoController.cs
@@ -110,7 +110,7 @@ namespace Projeto_Elite.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao")] Arquivo arquivo)
         {
             if (id != arquivo.Id)
